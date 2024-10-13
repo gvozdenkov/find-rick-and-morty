23bf6c15508bbeec1e2add9926938d2112c5ca26
arty gvozdenkov <gvozdenkov@gmail.com>
2024-10-13 07:58:11 +0300

    style: make form responsive

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -63,67 +63,69 @@ export default function Index() {
 
   return (
     <div className="flex gap-12 flex-col items-center justify-center">
-      <Form className="flex flex-col gap-6 w-full items-center" aria-label="Find charecter">
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="name">Charecter name</label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            className="p-2 bg-transparent border rounded-lg dark:border-white"
-            autoComplete="off"
-            defaultValue={nameDefault ?? undefined}
-          />
+      <Form className="flex flex-col gap-6 w-full" aria-label="Find charecter">
+        <div className="grid grid-cols-1 gap-6 items-center | sm:grid-cols-6 md:gap-y-2">
+          <div className="flex flex-col gap-2 w-full | sm:col-span-full">
+            <label htmlFor="name">Charecter name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="p-2 bg-transparent border rounded-lg dark:border-white"
+              autoComplete="off"
+              defaultValue={nameDefault ?? undefined}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full | sm:col-span-3 md:col-span-2">
+            <label htmlFor="status">Status</label>
+            <select
+              id="status"
+              name="status"
+              className="p-2 bg-transparent border rounded-lg dark:border-white"
+              defaultValue={statusDefault ?? undefined}
+            >
+              <option value=""></option>
+              <option value="alive">Alive</option>
+              <option value="dead">Dead</option>
+              <option value="unknown">unknown</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 w-full | sm:col-span-3 md:col-span-2">
+            <label htmlFor="species">Species</label>
+            <select
+              id="species"
+              name="species"
+              className="p-2 bg-transparent border rounded-lg dark:border-white"
+              defaultValue={speciesDefault ?? undefined}
+            >
+              <option value="" className="bg-black"></option>
+              <option value="human">Human</option>
+              <option value="animal">Animal</option>
+              <option value="humanoid">Humanoid</option>
+              <option value="alien">Alien</option>
+              <option value="robot">Robot</option>
+              <option value="cronenberg">Cronenberg</option>
+              <option value="disease">Disease</option>
+              <option value="poopybutthole">Poopybutthole</option>
+              <option value="mythological creature">Mythological Creature</option>
+              <option value="unknown">unknown</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 w-full | sm:col-span-full md:col-span-2">
+            <label htmlFor="episode">Episode</label>
+            <input
+              id="episode"
+              name="episode"
+              type="number"
+              min="1"
+              max="51"
+              className="p-2 bg-transparent border rounded-lg dark:border-white"
+              autoComplete="off"
+              defaultValue={episodeDefault ?? undefined}
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="status">Status</label>
-          <select
-            id="status"
-            name="status"
-            className="p-2 bg-transparent border rounded-lg dark:border-white"
-            defaultValue={statusDefault ?? undefined}
-          >
-            <option value=""></option>
-            <option value="alive">Alive</option>
-            <option value="dead">Dead</option>
-            <option value="unknown">unknown</option>
-          </select>
-        </div>
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="species">Species</label>
-          <select
-            id="species"
-            name="species"
-            className="p-2 bg-transparent border rounded-lg dark:border-white"
-            defaultValue={speciesDefault ?? undefined}
-          >
-            <option value="" className="bg-black"></option>
-            <option value="human">Human</option>
-            <option value="animal">Animal</option>
-            <option value="humanoid">Humanoid</option>
-            <option value="alien">Alien</option>
-            <option value="robot">Robot</option>
-            <option value="cronenberg">Cronenberg</option>
-            <option value="disease">Disease</option>
-            <option value="poopybutthole">Poopybutthole</option>
-            <option value="mythological creature">Mythological Creature</option>
-            <option value="unknown">unknown</option>
-          </select>
-        </div>
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="episode">Episode</label>
-          <input
-            id="episode"
-            name="episode"
-            type="number"
-            min="1"
-            max="51"
-            className="p-2 bg-transparent border rounded-lg dark:border-white"
-            autoComplete="off"
-            defaultValue={episodeDefault ?? undefined}
-          />
-        </div>
-        <button className="button" type="submit">
+        <button className="button | sm:self-center" type="submit">
           Find
         </button>
       </Form>
